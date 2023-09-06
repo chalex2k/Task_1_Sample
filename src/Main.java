9a83615fb437265611f732f7e758b1cbce1762cc
alex <chalex2k@gmail.com>
2023-09-06 20:08:31 +0300

    поправил коммент

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
 
 /*
-Таск <номер таска>
+Чеклист
 1. Определить входные данные, выходные данные, их тип
 2. Придумать интерфейс
 3. Реализовать ввод данных
-4. Решить задачу на листочке - получить формулы (пройтись по задачам и проговорить идеи)
+4. Решить задачу на листочке - получить формулы
 5. Перенести формулу(ы) в код
 6. Реализовать вывод результата
 7. Вынести расчёт в функцию
 8. Рефакторинг
  */
-
-import java.util.Locale;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,6 +19,7 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello World!");
+        double x = scanner.nextDouble();
+        System.out.println(x);
     }
 }
