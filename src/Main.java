502d464c69c4ec243f75bfffbb059d0aef97d5af
alex <chalex2k@gmail.com>
2023-09-06 09:12:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+/*
+Таск <номер таска>
+1. Определить входные данные, выходные данные, их тип
+2. Придумать интерфейс
+3. Реализовать ввод данных
+4. Решить задачу на листочке - получить формулы (пройтись по задачам и проговорить идеи)
+5. Перенести формулу(ы) в код
+6. Реализовать вывод результата
+7. Вынести расчёт в функцию
+8. Рефакторинг
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ROOT);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello World!");
+    }
+}
